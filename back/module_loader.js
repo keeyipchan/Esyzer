a05b85d22fa14a72c2d150213bc5fe5e9a2d2d48
Anton Ulianov <anton.ulianov@gmail.com>
2012-08-21 20:15:04 +0300

    first client-server communication

--- back/module_loader.js
+++ back/module_loader.js
@@ -1,23 +1,28 @@
 "use strict";
 var fs = require('fs');
 var config = require('../config.js');
+var q = require('q');
 
 var modules = undefined;
 
 function loadModuleList () {
     if (modules) return;
+    var def = q.defer();
     fs.readdir(config.srcPath, function (err, files) {
         if (err) {
             console.log('Error reading module files: ', err)
         } else {
             modules = files;
+            def.resolve();
         }
     });
+    return def.promise;
 }
 
 exports.getModuleList = function (req, res) {
-    loadModuleList();
-//    console.log('asd')
-    res.send([]);
+    loadModuleList().then(function () {
+        res.send(modules);
+    })
+
 };
 
